301ccdf24b40eeeb21d28adb0dc2092ef486be5d
tolikavr <anatolii13@mail.ru>
2022-01-24 16:31:08 +0300

    Add enqueue method to MyJobService.kt

--- app/src/main/java/com/tolikavr/servicestest/MainActivity.kt
+++ app/src/main/java/com/tolikavr/servicestest/MainActivity.kt
@@ -2,7 +2,9 @@ package com.tolikavr.servicestest
 
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
   private val binding by lazy {
     ActivityMainBinding.inflate(layoutInflater)
   }
+
+  private var page = 0
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -38,12 +42,14 @@ class MainActivity : AppCompatActivity() {
       val jobInfo = JobInfo.Builder(JOB_ID, componentName)
         .setRequiresCharging(true)
         .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-        .setPersisted(true)
-        .setPeriodic(1000)
         .build()
 
       val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-      jobScheduler.schedule(jobInfo)
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val intent = MyJobService.newIntent(page++)
+        jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+      }
     }
   }
 }
